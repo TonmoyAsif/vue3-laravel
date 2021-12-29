454db1c04c51c6caec9d18e2e1b585425c491833
Tonmoy Enkaizen <tonmoy@enkaizen.com>
2021-12-29 21:53:17 +0600

    Added Laravel IDE Helper

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,6 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 /**
  * App\Models\Product
  *
+ * @property int $id
+ * @property string $title
+ * @property string $price
+ * @property string $description
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|Product newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Product newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Product query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Product whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Product whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Product whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Product wherePrice($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Product whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Product whereUpdatedAt($value)
+ * @mixin \Eloquent
  */
 class Product extends Model
 {
